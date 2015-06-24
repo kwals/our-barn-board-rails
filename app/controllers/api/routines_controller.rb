1a3ae5e9d493fac47b8ccc9af33f90fde481d200
Katie Walsh <katie.katiewalsh@gmail.com>
2015-06-23 20:06:50 -0400

    Update files

--- app/controllers/api/routines_controller.rb
+++ app/controllers/api/routines_controller.rb
@@ -1,8 +1,8 @@
 class Api::RoutinesController < ApplicationController
   skip_before_action protect_from_forgery with: :exception 
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
     if Time.now.hour <= 12 #THIS IS LOCALTIME
